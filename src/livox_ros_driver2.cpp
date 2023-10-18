c2170139094b40abc27e59f2b9b23797b06e6a47
Mikkel Tiller <mikkeltiller@gmail.com>
2023-10-18 09:22:05 +0200

    Make params private

--- src/livox_ros_driver2.cpp
+++ src/livox_ros_driver2.cpp
@@ -45,7 +45,7 @@ int main(int argc, char **argv) {
 
   ros::init(argc, argv, "livox_lidar_publisher");
 
-  // ros::NodeHandle livox_node;
+  ros::NodeHandle private_nh{"~"};
   livox_ros::DriverNode livox_node;
 
   DRIVER_INFO(livox_node, "Livox Ros Driver2 Version: %s", LIVOX_ROS_DRIVER2_VERSION_STRING);
@@ -60,14 +60,14 @@ int main(int argc, char **argv) {
   bool lidar_bag = true;
   bool imu_bag   = false;
 
-  livox_node.GetNode().getParam("xfer_format", xfer_format);
-  livox_node.GetNode().getParam("multi_topic", multi_topic);
-  livox_node.GetNode().getParam("data_src", data_src);
-  livox_node.GetNode().getParam("publish_freq", publish_freq);
-  livox_node.GetNode().getParam("output_data_type", output_type);
-  livox_node.GetNode().getParam("frame_id", frame_id);
-  livox_node.GetNode().getParam("enable_lidar_bag", lidar_bag);
-  livox_node.GetNode().getParam("enable_imu_bag", imu_bag);
+  private_nh.getParam("xfer_format", xfer_format);
+  private_nh.getParam("multi_topic", multi_topic);
+  private_nh.getParam("data_src", data_src);
+  private_nh.getParam("publish_freq", publish_freq);
+  private_nh.getParam("output_data_type", output_type);
+  private_nh.getParam("frame_id", frame_id);
+  private_nh.getParam("enable_lidar_bag", lidar_bag);
+  private_nh.getParam("enable_imu_bag", imu_bag);
 
   printf("data source:%u.\n", data_src);
 
@@ -90,7 +90,7 @@ int main(int argc, char **argv) {
     DRIVER_INFO(livox_node, "Data Source is raw lidar.");
 
     std::string user_config_path;
-    livox_node.getParam("user_config_path", user_config_path);
+    private_nh.getParam("user_config_path", user_config_path);
     DRIVER_INFO(livox_node, "Config file : %s", user_config_path.c_str());
 
     LdsLidar *read_lidar = LdsLidar::GetInstance(publish_freq);
